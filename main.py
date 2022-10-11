619f9550f7873f4692a0196b98060112816c2f61
nm000 <79460564+nm000@users.noreply.github.com>
2022-10-11 14:27:14 -0500

    crea la tabla e ingresa el dataset
    Para que funcione debes colocar el nombre del servidor de tu management studio, además, crear en ese mismo servidor la base de datos llamada "covid19", el resto lo hace el .py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
-from flask_mysqldb import MySQL
+import pandas as p
+import pyodbc
 
 web = Flask(__name__)
 
-web.config['MYSQL_HOST'] = 'localhost'
-web.config['MYSQL_USER'] = 'sa'
-web.config['MYSQL_PASSWORD'] = '123456'
-web.config['MYSQL_DB'] = 'AdventureWorks2012_CS'
-sql = MySQL(web)
+report_data=p.read_csv('C:/Users/natyp/OneDrive/Escritorio/BASES DE DATOS/dataset20220808.csv', encoding='latin-1')
+values_list_data=report_data.values.tolist()
+
+server = 'LAPTOP-51FAGA1L'
+database_name='covid19'
+cnx=pyodbc.connect(driver='{SQL server}', host=server, database=database_name)
+print('succesfull conection')
 
 @web.after_request
 def after_request(response):
@@ -16,21 +19,16 @@ def after_request(response):
 
 @web.route('/')
 def principal():
-    search = sql.connection.cursor()
-    search.execute("""
-    with SumaT (IDTerr, suma) as (
-	select sale.TerritoryID, sum(sale.SubTotal) Suma
-	from Sales.SalesOrderHeader sale
-	group by sale.TerritoryID
-    )
-    select top 1 *
-    from Sales.SalesTerritory s join SumaT suma
-    on s.TerritoryID = suma.IDTerr
-    order by suma.suma desc
-    """)
-    user = search.fetchone()
-    print(user)
-    search.close()
+    
+    cursor=cnx.cursor()
+    cursor.execute("CREATE TABLE Dataset(IdCaso INT PRIMARY KEY, FechaNotificacion DATE, CodigoDepartamento INT, NombreDepartamento VARCHAR(255) , CodigoMunicipio INT, NombreMunicipio VARCHAR(255), Edad INT, Sexo VARCHAR(2), TipoContagio VARCHAR(50), UbicacionCaso VARCHAR(255), EstadoActual VARCHAR(255) NOT NULL, CodigoPaisDeViaje VARCHAR(50), NombrePaisDeViaje VARCHAR(255), FechaInicioSintomas VARCHAR(255), FechaMuerte VARCHAR(255), FechaDiagnostico VARCHAR(255), FechaRecuperacion VARCHAR(50), FechaCargueWeb DATE, TipoRecuperacion VARCHAR(255), PertenenciaEtnica VARCHAR(255), NombreGrupoEtnico VARCHAR(255))")
+
+    cursor.executemany("INSERT INTO Dataset VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",values_list_data)
+
+    cursor.commit()
+
+    print("The process is alredy finished.")
+
     return render_template('home.html')
 
 @web.route('/about')
@@ -44,3 +42,6 @@ def not_found(error):
 if __name__ == '__main__':
     web.register_error_handler(404, not_found)
     web.run(debug=True)
+
+
+cnx.close()
